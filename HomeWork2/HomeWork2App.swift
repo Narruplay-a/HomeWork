832a92cac0c04e44907ad974191b0a5660f4a895
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2021-07-12 18:55:38 +0300

    Initial Commit

---
+++ HomeWork2/HomeWork2App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork2App.swift
+//  HomeWork2
+//
+//  Created by Adel Khaziakhmetov on 12.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
